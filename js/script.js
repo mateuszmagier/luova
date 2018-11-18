7ee0bc94ecc371b3a7af5e28a310681a82f128a9
mateuszmagier <mateuszmagier@bitbucket.org>
2018-11-18 16:40:14 +0100

    Refactor code and change appearance of portfolio & team sections.

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(function () {
+
+    // smooth scrolling setup
+    const smoothScroll = new SmoothScroll('a[href*="#"]', {
+        speed: 800
+    });
+
+    const collapsedNavMedia = window.matchMedia("(min-width: 601px)");
+
+    // showing & hiding navigation (width < 600px)
+    $('#showNavButton').click(function () {
+        if ($(this).hasClass('collapsed')) { // user is showing navigation
+            $('.navigation').slideDown();
+            $(this).removeClass('collapsed');
+        } else { // user is collapsing navigation
+            $('.navigation').slideUp();
+            $(this).addClass('collapsed');
+        }
+    });
+
+    collapsedNavMedia.addListener(function (media) {
+        if (media.matches && $('#showNavButton').hasClass('collapsed')) {
+            $('.navigation').slideDown();
+        }
+    });
+});
